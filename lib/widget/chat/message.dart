d45ea724906e497b76dc57296d4442999f6a8037
ngweijian1 <70877795+ngweijian1@users.noreply.github.com>
2021-08-05 22:50:47 +0800

    chat1

--- lib/widget/chat/message.dart
+++ lib/widget/chat/message.dart
@@ -2,6 +2,7 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dating_app/widget/chat/message_bubble.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 
@@ -9,6 +10,7 @@ class Messages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    User currentUser = FirebaseAuth.instance.currentUser!;
     return StreamBuilder(stream: FirebaseFirestore.instance.collection('chat').orderBy('times', descending: true,).snapshots(),
     builder: (ctx, AsyncSnapshot<QuerySnapshot> chatSnapshot){
        if (chatSnapshot.connectionState ==
@@ -18,11 +20,18 @@ class Messages extends StatelessWidget {
                     );
                   }
                   final chatdocuments = chatSnapshot.data!.docs;
-                  return ListView.builder(
-                    reverse: true,
-                    itemCount: chatdocuments.length,
-                    itemBuilder: (ctx, index) => MessageBubble(chatdocuments[index]['text']),
-                  );
-    },);
-  }
+                  
+                      return ListView.builder(
+                      reverse: true,
+                      itemCount: chatdocuments.length,
+                      itemBuilder: (ctx, index) => MessageBubble(
+                        chatdocuments[index]['text'], 
+                        chatdocuments[index]['userid'] == currentUser.uid,
+                        key: ValueKey(chatdocuments[index].id),
+                        ),
+                    );
+                    
+    },
+    );
+  } 
 }
