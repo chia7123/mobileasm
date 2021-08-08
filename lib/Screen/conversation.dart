9c9bf2fc656f2fde2a659a3e30d60852c7070b35
ngweijian1 <70877795+ngweijian1@users.noreply.github.com>
2021-08-08 20:34:45 +0800

    23

--- lib/Screen/conversation.dart
+++ lib/Screen/conversation.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-
 class ConversationScreen extends StatefulWidget {
-
   @override
   _ConversationScreenState createState() => _ConversationScreenState();
 }
@@ -10,8 +8,56 @@ class ConversationScreen extends StatefulWidget {
 class _ConversationScreenState extends State<ConversationScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        child: Stack(
+          children: [
+            Container(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                color: Colors.grey,
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: TextField(
+                      //controller: searchtextcontol,
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                          hintText: "Enter Message here",
+                          hintStyle: TextStyle(color: Colors.white),
+                          border: InputBorder.none),
+                    )),
+                    GestureDetector(
+                      onTap: () async {
+//print("${Constants.myName} 1");
+                        //print("${ await HelperFunctions.getuserNameSharedPreference()} 123");
+            
+                        //initiateSearch();
+                        FocusScope.of(context).unfocus();
+                        //searchtextcontol.clear();
+                      },
+                      child: Container(
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(colors: [
+                              const Color(0x36FFFFFF),
+                              const Color(0x0FFFFFFF),
+                            ]),
+                            borderRadius: BorderRadius.circular(40),
+                          ),
+                          padding: EdgeInsets.all(10),
+                          child: Image.asset("assets/images/search_white.png")),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
