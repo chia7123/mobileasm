bb8f70d292d7b1b760f2f95436d05cdb941ab233
chia7123 <84016807+chia7123@users.noreply.github.com>
2021-08-11 16:27:08 +0800

    Update welcomePage.dart

--- lib/welcomePage.dart
+++ lib/welcomePage.dart
@@ -1,10 +1,8 @@
 import 'package:dating_app/Screen/chat_room.dart';
-import 'package:dating_app/Screen/chat_screen.dart';
 import 'package:dating_app/authentication/auth.dart';
-import 'package:dating_app/authentication/google_signin.dart';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({Key key}) : super(key: key);
