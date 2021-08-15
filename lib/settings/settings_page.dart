a98ae63f51c8000d8e55feb07a0eddac66c3540d
heyemi99 <jeremyhewm@gmail.com>
2021-08-16 01:23:04 +0800

    Create settings_page.dart

---
+++ lib/settings/settings_page.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: SettingPageUI(),
+    );
+  }
+}
+
+class SettingPageUI extends StatefulWidget {
+  @override
+  _SettingPageUIState createState() => _SettingPageUIState();
+}
+
+class _SettingPageUIState extends State<SettingPageUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Settings", style: TextStyle(fontSize: 22)),
+        leading: IconButton(
+          onPressed: (){},
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: [
+            SizedBox(height: 40),
+            Row(
+              children: [
+                Icon(
+                  Icons.person,
+                  color: Colors.blue,
+                ),
+                SizedBox(width: 10),
+                Text("Account", style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold))
+              ],
+            ),
+            Divider(height: 20, thickness: 1),
+            SizedBox(height: 10),
+            buildAccountOption(context, "Change Password"),
+            buildAccountOption(context, "Update Email"),
+            buildAccountOption(context, "Update Preference"),
+          ],
+        ),
+      ),
+    );
+  }
+
+  GestureDetector buildAccountOption(BuildContext context, String title) {
+    return GestureDetector(
+      onTap: () {
+        showDialog(context: context, builder: (BuildContext context){
+          return AlertDialog(
+            title: Text(title),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text("INSERT HERE")
+              ],
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                }, 
+                child: Text("Close"))
+            ],
+          );
+        });
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(title, style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+              color: Colors.grey[600]
+            )),
+            Icon(Icons.arrow_forward_ios,
+            color: Colors.grey
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
