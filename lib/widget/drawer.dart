f56621d9d143327c0822e75c9dd7272feeb52fbd
ben2ooo <88441626+ben2ooo@users.noreply.github.com>
2021-08-20 15:12:35 +0800

    Ben final 4

--- lib/widget/drawer.dart
+++ lib/widget/drawer.dart
@@ -1,70 +1,117 @@
-import 'package:dating_app/Settings/theme.dart';
+
+import 'package:dating_app/database/database.dart';
 import 'package:dating_app/location_service_huawei/encounter_list.dart';
-import 'package:dating_app/location_service_huawei/global.dart';
-import 'package:dating_app/widget/dialogBox.dart';
 import 'package:dating_app/widget/user_profile.dart';
 import 'package:dating_app/wrapper.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_phoenix/flutter_phoenix.dart';
 
-class Drawers extends StatelessWidget {
+bool dark_mode=true;
+
+Color PRIMARY_COLOR = dark_mode? Color(0xFFEEEEEE): Color(0xFF035AA6);
+Color SECONDARY_COLOR = dark_mode?  Color(0xFF035AA6):Color(0xFFEEEEEE);
+
+class Drawers extends StatefulWidget {
   const Drawers({Key key}) : super(key: key);
 
-  Widget buildListTile(String text, IconData icon, Function tapHandler) {
-    return ListTile(
-      leading: Icon(
-        icon,
-        size: 26,
-      ),
-      title: Text(
-        text,
-        style: TextStyle(
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      onTap: tapHandler,
-    );
-  }
+  @override
+  _DrawersState createState() => _DrawersState();
+}
 
+class _DrawersState extends State<Drawers> {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Column(
-        children: [
-          Container(
-            height: 100,
-            width: double.infinity,
-            padding: const EdgeInsets.all(20),
-            alignment: Alignment.centerLeft,
-            color: AppColors.SECONDARY_COLOR,
-            child: Text(
-              'Settings',
-              style: TextStyle(
-                fontWeight: FontWeight.w900,
-                fontSize: 30,
-                color: Colors.white,
+      child: Container(
+        color: PRIMARY_COLOR,
+        child: Column(
+          children: [
+            Container(
+              height: 100,
+              width: double.infinity,
+              padding: const EdgeInsets.all(20),
+              alignment: Alignment.centerLeft,
+              color: SECONDARY_COLOR,
+              child: Text(
+                'Settings',
+                style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 30,
+                  color: PRIMARY_COLOR,
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          buildListTile('Account', Icons.account_box, () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => user_Profile()));
-          }),
-          buildListTile('Encounter List', Icons.list, () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => encounter_list_page()));
-          }),
-          buildListTile('Logout', Icons.logout, () {
-            FirebaseAuth.instance.signOut();
-            Navigator.pushReplacement(context,
-                MaterialPageRoute(builder: (context) => Wrapper()));
-          }),
-        ],
+            SizedBox(
+              height: 20,
+            ),
+
+            Container(
+              color: PRIMARY_COLOR,
+              child: Column(
+                children: [
+                  buildListTile('Account', Icons.account_box, () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => user_Profile()));
+                  }),
+                  buildListTile('Encounter List', Icons.list, () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => encounter_list_page()));
+                  }),
+                  ListTile(
+                    leading: Icon(
+                      Icons.dark_mode,
+                      size: 26,
+                      color: SECONDARY_COLOR,
+                    ),
+                    title: Switch(
+                      value: dark_mode,
+                      onChanged: (value) {
+                        setState(() {
+                          Phoenix.rebirth(context);
+
+
+                          SECONDARY_COLOR = dark_mode? Color(0xFFEEEEEE): Color(0xFF035AA6);
+                          PRIMARY_COLOR = dark_mode?  Color(0xFF035AA6):Color(0xFFEEEEEE);
+
+                          dark_mode = value;
+                          print(dark_mode);
+                        });
+                      },
+                      activeTrackColor: SECONDARY_COLOR,
+                      activeColor: PRIMARY_COLOR,
+                    ),
+                  ),
+                  buildListTile('Logout', Icons.logout, () {
+                    FirebaseAuth.instance.signOut();
+                    Navigator.pushReplacement(
+                        context, MaterialPageRoute(builder: (context) => Wrapper()));
+                  }),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
+}
+
+Widget buildListTile(String text, IconData icon, Function tapHandler) {
+  return ListTile(
+    leading: Icon(
+      icon,
+      size: 26,
+      color: SECONDARY_COLOR,
+    ),
+    title: Text(
+      text,
+      style: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+        color: SECONDARY_COLOR
+      ),
+    ),
+    onTap: tapHandler,
+  );
 }
